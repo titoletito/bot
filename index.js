4d03a22921dafa30e1bd1eaca144c9ee09a26f31
anonymocraft <33197223+anonymocraft@users.noreply.github.com>
2018-01-17 23:39:57 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const client = new Discord.Client
 client.login(process.env.TOKEN)
 
 client.on('ready', function() {
-	client.user.setGame('vous surveiller.[-help]')
+	client.user.setGame('vous surveiller. [-help]')
 	console.log('Bot Ready!') } )
 
 client.on('message', function (message) {
