a75aa41450b3f1e8a71e1488d85597884d611343
anonymocraft <33197223+anonymocraft@users.noreply.github.com>
2018-01-17 19:24:23 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+const Discord = require('discord.js')
+const client = new Discord.Client
+
+
+client.login('MzM1NDU5OTYxMDQxNTg0MTI5.DUEBbw.zulmiqh8iF1Eg4oyTToy2Bc3cYM')
+
+client.on('message', function (message) {
+	if ( message.content === '-message' ) {
+	message.reply('Dsl tu est maigre, trop maigre....') }
+	if ( message.content === '-invisible' && channel.user.id === '335118921088630796' ) {
+	Presence.status(invisible)  }
+})
