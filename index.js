d0698a87655446479506d95ef0077f9092c6cbf3
anonymocraft <33197223+anonymocraft@users.noreply.github.com>
2018-01-18 00:05:55 +0100

    Update index.js

--- index.js
+++ index.js
@@ -12,6 +12,6 @@ client.on('message', function (message) {
 	if ( message.content === '-test' ) {
 	message.channel.send('Tout est OK, enfin pour l\'instant.') } 
 	if ( message.content === '-help' ) {
-	message.channel.send('Voici la liste des commandes disponibles : \n -test : si le bot repond c\'est que tout est OK \n -help : affiche le menu d\'aide' \n -source-code : affiche le code source.') } 
+	message.channel.send('Voici la liste des commandes disponibles : \n -test : si le bot repond c\'est que tout est OK \n -help : affiche le menu d\'aide' \n -source-code : affiche le code source.') }
 	if ( message.content === '-source-code') {
-	message.channel.send('Le code source est disponible à l\'adresse suivante : https://github.com/anonymocraft/bot/')  } } )
+	message.channel.send('Le code source est disponible à l\'adresse suivante : https://github.com/anonymocraft/bot/') } } )
