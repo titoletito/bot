9ee24dfc041663954b9bc91822ebec8e0e453a7b
anonymocraft <33197223+anonymocraft@users.noreply.github.com>
2018-01-17 23:03:43 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ client.login(process.env.TOKEN)
 
 client.on('message', function (message) {
 	if ( message.content === '-test' ) {
-	message.channel.send('Tout est OK ') }
+	message.channel.send('Tout est OK, enfin pour l\'instant') }
 	if ( message.content === '-message' ) {
 	message.reply('Dsl tu est maigre, trop maigre....') }
 	if ( message.content === '-invisible' && channel.user.id === '335118921088630796' ) {
