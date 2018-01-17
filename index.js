6d4e53d8b24edc9188d36e49750b7de79de2b1af
anonymocraft <33197223+anonymocraft@users.noreply.github.com>
2018-01-17 23:35:21 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ client.login(process.env.TOKEN)
 
 client.on('ready', function() {
 	client.user.setGame('vous surveiller.[-help]')
-	console.send('Bot Ready!') } )
+	console.log('Bot Ready!') } )
 
 client.on('message', function (message) {
 	if ( message.content === '-test' ) {
 	message.channel.send('Tout est OK, enfin pour l\'instant.') }
-})
+}
