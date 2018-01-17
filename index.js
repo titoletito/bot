5fb7fbdf78d4a94845ea4a4551db7ffb1a1e3dc1
anonymocraft <33197223+anonymocraft@users.noreply.github.com>
2018-01-17 20:34:39 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js')
 const client = new Discord.Client
 
 
-client.login('MzM1NDU5OTYxMDQxNTg0MTI5.DUEBbw.zulmiqh8iF1Eg4oyTToy2Bc3cYM')
+client.login('')
 
 client.on('message', function (message) {
 	if ( message.content === '-message' ) {
